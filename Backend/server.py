2c3ab184ffe55faa634c1e00f9a0b2c0bf8eb6d3
MattiasKDev <mattias@kerner.ca>
2025-01-18 17:57:30 -0500

    fixed some imports and shit ig

--- Backend/server.py
+++ Backend/server.py
@@ -1,14 +1,15 @@
 import json
 
 from flask import Flask, request
-
 from Pandas.Code.cleaning import process_data
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello, World!"
+
 
 @app.route("/api/analyze", methods=["POST"])
 def analyze():
@@ -17,8 +18,9 @@ def analyze():
     with open("Backend/Pandas/Data/data.json", "w") as f:
         json.dump(data, f)
     # send to other functions for whatever analysis and then return
-    
+
     return {"message": process_data("Backend/Pandas/Data/data.json")}
+
 
 if __name__ == "__main__":
     # app.run(port=5001)
