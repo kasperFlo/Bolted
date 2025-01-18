0cfc2825b7db89a9a912de75264673548f5b5b28
Florian Kasperbauer <20kasperbauefr2@gmail.com>
2025-01-18 01:01:54 -0500

    base extension navigation is done

---
+++ Extension/FullScreen.js
@@ -0,0 +1,1 @@
+console.log('FullScreen.js loaded');
--- Extension/popup.js
+++ Extension/popup.js
@@ -1,1 +1,6 @@
-console.log('This is a popup!');
+document.getElementById('openTab').addEventListener('click', function() {
+    chrome.tabs.create({ url: 'fullpage.html' });
+  });
+  
+
+  console,log('popup.js loaded');
